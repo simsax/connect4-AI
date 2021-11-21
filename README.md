6188257189461e730c44be77cd86aedaa0959852
Simone Sasso <simsax98@gmail.com>
2021-11-21 20:10:09 +0100

    nearly finished

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # connect4-AI
 Connect 4 lab for Computational Intelligence
+
+## Things I tried
+
+- MTCS (not used because didn't work very well, but the problem could be my implementation)
+- Alpha-beta pruning
+- Bitboard to represent the game board instead of a 2d array
+- Reordering of columns to increase chance of pruning (center columns are better in general)
+- Saving the states in a dictionary since different plays can end up in the same configurations
+- Bonus: I tried to use John Tromp's database (https://www.openml.org/d/40668) for the openings, but I noticed that some 'draw' configurations are missing (not only the ones where the next move is forced) so for now I return 0 if a configuration is not found in the table.
--- connect4.py
+++ connect4.py
@@ -1,183 +1,210 @@
-from collections import Counter
+from collections import Counter, deque
+from collections import OrderedDict
 import numpy as np
 import sys, pygame
+
+AI_VS_AI = False # set to True to let the AI play against itself
+START_PLAYER = 1 # 1 is AI, 0 is user
+DEPTH = 4 # increase to have more precise AI, but longer computational time
 
 NUM_COLUMNS = 7
 COLUMN_HEIGHT = 6
 FOUR = 4
-
-# Board can be initiatilized with `board = np.zeros((NUM_COLUMNS, COLUMN_HEIGHT), dtype=np.byte)`
-# Notez Bien: Connect 4 "columns" are actually NumPy "rows"
+MAX_TABLE_SIZE = 10000
 
 def valid_moves(board):
-    """Returns columns where a disc may be played"""
-    return [n for n in range(NUM_COLUMNS) if board[n, COLUMN_HEIGHT - 1] == 0]
+    moves = []
+    TOP = 0b1000000_1000000_1000000_1000000_1000000_1000000_1000000
+    for i in range(NUM_COLUMNS):
+        if TOP & (1 << board[2][i]) == 0: # shifting height by one should not end up in the top row if not full
+            moves.append(i)
+    return moves
 
+def play(board, col, player):
+    move = 1 << board[2][col] # left shift 1 of height[col] positions. es height[col]=2 -> 100 (the 1 corresponds to the first free element in a column)
+    board[2][col] += 1
+    board[player] ^= move
 
-def play(board, column, player):
-    """Updates `board` as `player` drops a disc in `column`"""
-    (index,) = next((i for i, v in np.ndenumerate(board[column]) if v == 0)) # index of first element with value 0 in the column
-    board[column, index] = player
+def four_in_a_row(bitboard):
+    directions = [1, 7, 6, 8] # | - \ /
+    for dir in directions:
+        temp = bitboard & (bitboard >> dir)
+        if temp & (temp >> 2 * dir):
+            return True
+    return False
 
+def take_back(board, col, player):
+    board[2][col] -= 1
+    move = 1 << board[2][col]
+    board[player] ^= move
 
-def take_back(board, column):
-    """Updates `board` removing top disc from `column`"""
-    (index,) = [i for i, v in np.ndenumerate(board[column]) if v != 0][-1] # index of last non-zero element in the column
-    board[column, index] = 0
-
-
-def four_in_a_row(board, player):
-    """Checks if `player` has a 4-piece line"""
-    return (
-        any(
-            all(board[c, r] == player)
-            for c in range(NUM_COLUMNS)
-            for r in (list(range(n, n + FOUR)) for n in range(COLUMN_HEIGHT - FOUR + 1))
-        )
-        or any(
-            all(board[c, r] == player)
-            for r in range(COLUMN_HEIGHT)
-            for c in (list(range(n, n + FOUR)) for n in range(NUM_COLUMNS - FOUR + 1))
-        )
-        or any(
-            np.all(board[diag] == player)
-            for diag in (
-                (range(ro, ro + FOUR), range(co, co + FOUR))
-                for ro in range(0, NUM_COLUMNS - FOUR + 1)
-                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
-            )
-        )
-        or any(
-            np.all(board[diag] == player)
-            for diag in (
-                (range(ro, ro + FOUR), range(co + FOUR - 1, co - 1, -1))
-                for ro in range(0, NUM_COLUMNS - FOUR + 1)
-                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
-            )
-        )
-    )
-
-## Montecarlo evalutaion
 def _mc(board, player):
-    p = -player
+    p = int(not player)
+    moves = deque()
+    found = False
     while valid_moves(board):
-        p = -p
+        p = int(not p)
         c = np.random.choice(valid_moves(board))
         play(board, c, p)
-        if four_in_a_row(board, p):
-            return p
-    return 0
-
+        moves.append((c, p))
+        if four_in_a_row(board[p]):
+            found = True
+            break
+    while moves:
+        col, player = moves.pop()
+        take_back(board, col, player)
+    if found:
+        return p
+    else:
+        return -1 # draw
 
 def montecarlo(board, player):
     montecarlo_samples = 100
-    cnt = Counter(_mc(np.copy(board), player) for _ in range(montecarlo_samples))
-    return (cnt[1] - cnt[-1]) / montecarlo_samples # score representing who is probably going to win (-1 is a certain win for player -1, 0 is 50%, 1 is 100% win for player 1)
-
-def my_montecarlo(board, player):
-    montecarlo_samples = 100
-    cnt = Counter(_mc(np.copy(board), player) for _ in range(montecarlo_samples))
-    return (cnt[player] - cnt[-player]) / montecarlo_samples # score representing if player is probably going to win (-1 is a certain win for opponent, 0 is 50%, 1 is 100% win for player)
-
+    cnt = Counter(_mc((board), player) for _ in range(montecarlo_samples))
+    return (cnt[player] - cnt[int(not player)]) / montecarlo_samples 
 
 def eval_board(board, player):
-    if four_in_a_row(board, 1):
-        # Alice won
+    if four_in_a_row(board[player]):
         return 1
-    elif four_in_a_row(board, -1):
-        # Bob won
+    elif four_in_a_row(board[int(not player)]):
         return -1
     else:
         # Not terminal, let's simulate...
         return montecarlo(board, player)
 
-def my_eval_board(board, player):
-    if four_in_a_row(board, player):
-        # Alice won
+# To increase pruning chances I explore first the center columns, which are better than edge columns on average
+def reorder(moves):
+    ideal = np.array([3,4,2,1,5,6,0])
+    return ideal[np.in1d(ideal, moves, assume_unique=True)]
+
+def mandatory_move(moves, board, player): # immediate loss or win
+    bottom_row_opponent = board[int(not player)] & 34630287489 # 2216338399296 # mask to have the values in the bottom row
+    if bottom_row_opponent == 270532608:
+        return 2
+    elif bottom_row_opponent == 34628173824:
         return 1
-    elif four_in_a_row(board, -player):
-        # Bob won
-        return -1
-    else:
-        # Not terminal, let's simulate...
-        return my_montecarlo(board, player)
+    for ply in moves:
+        play(board, ply, int(not player))
+        if four_in_a_row(board[int(not player)]):
+            take_back(board, ply, int(not player))
+            return ply
+        take_back(board, ply, int(not player))
+        play(board, ply, player)
+        if four_in_a_row(board[player]): # win move
+            take_back(board, ply, player)
+            return ply
+        take_back(board, ply, player)
+    return -1
 
-def alpha_beta(board, player, depth, alpha, beta):
-    possible = valid_moves(board)
-    if depth == 0 or not possible:
+
+def alpha_beta(board, player, depth, alpha, beta, table):
+    possible = reorder(valid_moves(board))
+    key = str(board[player]) + str(board[int(not player)])
+    if key in table:
+        return table[key]    
+    if depth == 0 or not any(possible):
         return None, eval_board(board, player)
-    
-    if player == 1:
-        max_eval = (None, -1)
-        for ply in possible:
-            play(board, ply, player)
-            _, val = alpha_beta(board, -player, depth - 1, alpha, beta) #  | is the union set operator
-            val = -val
-            take_back(board, ply)
-            if val > max_eval[1]:
-                max_eval = (ply, val)
-            alpha = max(alpha, val)
-            if beta <= alpha:
-                break
-        return max_eval
-    else:
-        min_eval = (None, 1)
-        for ply in possible:
-            play(board, ply, player)
-            _, val = alpha_beta(board, -player, depth - 1, alpha, beta) #  | is the union set operator
-            val = -val
-            take_back(board, ply)
-            if val < min_eval[1]:
-                min_eval = (ply, val)
-            beta = min(beta, val)
-            if beta <= alpha:
-                break
-        return min_eval
-
-def minmax(board, player, depth):
-    #val = 1 if four_in_a_row(board, player) else -1 if four_in_a_row(board, -player) else 0
-    possible = valid_moves(board)
-    if depth == 0 or not possible:
-        return None, my_eval_board(board, player)
-    evaluations = list()
+    imp_move = mandatory_move(possible, board, player)
+    if imp_move != -1:
+        return (imp_move, 1)
+    max_eval = (None, -1)
     for ply in possible:
         play(board, ply, player)
-        _, val = minmax(board, -player, depth - 1) #  | is the union set operator
-        take_back(board, ply)
-        evaluations.append((ply, -val))
-    return max(evaluations, key=lambda k: k[1])
+        _, val = alpha_beta(board, int(not player), depth - 1, -beta, -alpha, table)
+        val = -val
+        take_back(board, ply, player)
+        if max_eval[0] is None or val > max_eval[1]:
+            max_eval = (ply, val)
+        alpha = max(alpha, val)
+        if beta <= alpha:
+            break
+    table[key] = max_eval
+    if len(table) > MAX_TABLE_SIZE:
+        [table.popitem(last=False) for _ in range(100)] # remove oldest 100 items      
+    return max_eval
 
-board = np.zeros((NUM_COLUMNS, COLUMN_HEIGHT), dtype=np.byte)
-'''
-player = 1
-won = False
-while not won:
-    col = np.random.choice(valid_moves(board))
-    play(board,col,player)
-    won = four_in_a_row(board, player)
-    player = -player
-take_back(board, col)
-#print(col, -player)
-player = -player
-'''
+def alpha_beta_opening(board, player, depth, alpha, beta, table, openings_data):
+    possible = reorder(valid_moves(board))
+    key = str(board[player]) + str(board[int(not player)]) 
+    imp_move = mandatory_move(possible, board, player)
+    if imp_move != -1:
+        return (imp_move, 1)
+    if depth == 0 or not any(possible):
+        if key in openings_data:
+            return None, openings_data[key]
+        else:
+            return None, 0
+    max_eval = (None, -1)
+    for ply in possible:
+        play(board, ply, player)
+        _, val = alpha_beta_opening(board, int(not player), depth - 1, -beta, -alpha, table, openings_data)
+        val = -val
+        take_back(board, ply, player)
+        if max_eval[0] is None or val > max_eval[1]:
+            max_eval = (ply, val)
+        alpha = max(alpha, val)
+        if beta <= alpha:
+            break     
+    return max_eval
+
+
+def load_database(filename):
+    data = dict()
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip().split(',')
+            val = line.pop()
+            if val == 'win':
+                val = 1
+            elif val == 'loss':
+                val = -1
+            else:
+                val = 0
+    
+            board = [0,0]
+            for count, p in enumerate(reversed(line)):
+                board[0] = board[0] << 1
+                board[1] = board[1] << 1
+                if p == 'x':
+                    board[0] |= 1
+                elif p == 'o':
+                    board[1] |= 1
+                if (count+1) % 6 == 0 and count != 41: # additional shift
+                    board[0] = board[0] << 1
+                    board[1] = board[1] << 1
+            key = str(board[0]) + str(board[1])
+            data[key] = val
+    return data
+
+openings_data = load_database('connect-4.data') # openings database: https://www.openml.org/d/40668
 
 pygame.init()
 pygame.display.set_caption('Connect 4')
 size = width, height = (800, 600)
 board_size = (637,550)
 screen = pygame.display.set_mode(size)
-board_img = pygame.image.load('connect4assets/board.png')
-board_img = pygame.transform.scale(board_img, board_size)
+board_img = pygame.image.load('board.png')
+board_img = pygame.transform.smoothscale(board_img, board_size)
 boardrect = board_img.get_rect()
 white = (255, 255, 255)
 red = (230, 30, 30)
+light_red = (255, 153, 153)
 yellow = (234, 204, 73)
 
 step = 86
 radius = 40
 start_pos = (144, 86)
 
-def draw(board):
+def draw(board, click=False):
+    # convert the bitboard to 2d array
+    top = {6,13,20,27,34,41}
+    bits0 = [(board[0] >> i) & 1 for i in range(0,48) if i not in top]
+    bits1 = [-((board[1] >> i) & 1) for i in range(0,48) if i not in top]
+    board = np.add(np.reshape(bits0, (7,6)), np.reshape(bits1, (7,6)))
+
+    x_mouse = (pygame.mouse.get_pos()[0] - start_pos[0] + step/2) // step
+    screen.fill(white)
+    first = True
     for j in range(board.shape[1]):
         for i in range(board.shape[0]):
             if board[i][j] == 1:
@@ -185,43 +212,119 @@ def draw(board):
             elif board[i][j] == -1:
                 pygame.draw.circle(screen, yellow, (start_pos[0] + i*step, start_pos[1] + (5-j)*step), radius)
 
+            if not AI_VS_AI and not click and x_mouse == i and board[i][j] == 0 and first:
+                pygame.draw.circle(screen, light_red, (start_pos[0] + i*step, start_pos[1] + (5-j)*step), radius)
+                first = False
+    screen.blit(board_img, (81,25))
+    pygame.display.flip()
 
-def run():
-    pass
+board = [0,0, [0,7,14,21,28,35,42]] # [p1, p2, height]
+table = OrderedDict() # ordered dict so I can remove the LRU configuration
+player = START_PLAYER
 
-play(board, 4, 1)
-play(board, 0, -1)
-play(board, 5, 1)
-play(board, 0, -1)
-play(board, 6, 1)
-print(board)
-player = -1
-
-won = False
-screen.fill(white)
+finish = False
 draw(board)
-screen.blit(board_img, (81,25))
-pygame.display.flip()
 
+first = True
+n = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and AI_VS_AI == False and not finish:
+            if first:
+                first = False
+            left = pygame.mouse.get_pressed()[0]
+            if left and player == 0:
+                col = (pygame.mouse.get_pos()[0] - start_pos[0] + step/2) // step
+                if col in valid_moves(board):
+                    play(board, int(col), player)
+                    player = int(not player)
+                    draw(board, click=True)
 
-    while not won:        
-        #best_move, _ = minmax(board, player, 2)
-        best_move, _ = alpha_beta(board, player, 2, -2, 2)
-        play(board, best_move, player)
-        won = four_in_a_row(board, player) or four_in_a_row(board, -player)
-        player = -player
-        if won:
-            print(board)
+    if (AI_VS_AI or player == 1) and not finish:
+        if player:
+            n += 1
+        best_move = None
+        if first:
+            best_move = 3 # middle column
+            first = False
+        elif n <= 4: 
+            best_move, _ = alpha_beta_opening(board, player, 7, -np.inf, np.inf, openings_data)
+        else:
+            best_move, _ = alpha_beta(board, player, DEPTH, -np.inf, np.inf, table)
+        if best_move is None:
+            print(f"Draw")
+            finish = True
+        else:
+            play(board, best_move, player)
+            if four_in_a_row(board[0]):
+                print(f"Red won")
+                finish = True
+            if four_in_a_row(board[1]):
+                print(f"Yellow won")
+                finish = True
+            player = int(not player)
+    draw(board)
 
-        screen.fill(white)
-        draw(board)
-        screen.blit(board_img, (81,25))
-        pygame.display.flip()
-        
 
-# neat con board come input, 7 neuroni output (indicano la colonna in cui piazzare). Fitness formula tiene conto della vittoria più rapida, più una penalità per ogni sconfitta credo
+# Montecarlo Tree Search  - implemented but not used, works on professor's data structure (2d array for the board)
+'''
+def UCB(val, n, N):
+    return val + 2*np.sqrt(np.log(N)/n)
+
+class Node:
+    def __init__(self, t, n, parent, move):
+        self.t = t
+        self.n = n
+        self.parent = parent
+        self.move = move
+
+# works with old data structure where board is a 2d array
+def mcts(visited, board, player, current_player):
+    possibilities = valid_moves(board)
+    if not possibilities:
+        return
+    next = None
+    nodep = visited[board.tobytes()]
+    for ply in possibilities:
+        play(board, ply, current_player)
+        if board.tobytes() not in visited:
+            next = np.copy(board)
+            take_back(board, ply)
+            break
+        elif next is None or UCB(visited[board.tobytes()].t, visited[board.tobytes()].n, nodep.n) > UCB(visited[next.tobytes()].t, visited[next.tobytes()].n, nodep.n):
+            next = np.copy(board)
+        take_back(board, ply)
+    if next.tobytes() not in visited: # rollout
+        val = _mc(np.copy(next), current_player)
+        if player == -1:
+            val = -val
+        visited[next.tobytes()] = Node(val, 1, board.tobytes(), ply)
+        # backpropagation
+        next = next.tobytes()
+        while visited[next].parent is not None:
+            parent = visited[next].parent
+            visited[parent].t += val
+            visited[parent].n += 1
+            next = parent
+    else: # expansion
+        mcts(visited, next, player, -player)
+
+def MCTS(board, player, iterations):
+    visited = dict()
+    visited[board.tobytes()] = Node(0, 0, None, 0)
+    nodep = visited[board.tobytes()]
+    for _ in range(iterations):
+        mcts(visited, np.copy(board), player, player)
+    best = None
+    for ply in valid_moves(board):
+        play(board, ply, player)
+        next = visited[board.tobytes()]
+        #print(f"col: {next.move}, val: {next.t}, n: {next.n}")
+        if best is None or UCB(next.t, next.n, nodep.n) > UCB(best.t, best.n, nodep.n):
+            best = next
+        take_back(board, ply)
+    return best.move
+'''
